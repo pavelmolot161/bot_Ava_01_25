03274add4389a25822a736f7061dfd35bdadf724
pavelmolot161 <pavel.molot.161@yandex.ru>
2025-01-12 18:18:22 +0300

    Настроены все навигационные кнопки 12.01.25

---
+++ Chernovik.py
@@ -0,0 +1,11 @@
+
+### - Use this token to access the HTTP API:8150838104:AAGqtVnS80fWbp1iXidvJ5T3chYUTqLMOdc
+
+### - 1) - через pip install устанавливаем aiogram
+# (это библиотека для создания ботов, а версия считается более устойчивой)
+#                                                   >>> pip install aiogram==2.25.1
+
+### - 2) Создание файла зависимостей:
+#            (.venv) D:\Rabota_2025\bot_Ava_01_25.git git:[main] >>> pip freeze > requirements.txt
+#     2.a) вывод зависимостей в консоль:
+#                                                                       >>> pip freeze
